38ba9fd3bc1556bb148eb9e27a2261e7b4a3aca9
KC Kern <kc@kckern.com>
2023-09-18 13:35:50 +0900

    postprocess reply

--- src/api/translate.js
+++ src/api/translate.js
@@ -34,47 +34,6 @@ const loadTranslations = async (lang, items, guidkey = "guid") => {
 
 
 
-function translateReferences(lang, text)
-{
-    if(lang !== "ko") return text;
-    const dictionary = {
-        "1 Nephi":{  "ko": "니파이전서" },
-        "1 Ne\\.":{  "ko": "니파이전서" },
-        "2 Nephi":{  "ko": "니파이후서" },
-        "2 Ne\\.":{  "ko": "니파이후서" },
-        "Jacob":{  "ko": "야곱서" },
-        "Enos":{  "ko": "이노스서" },
-        "Jarom":{  "ko": "예이롬서" },
-        "Omni":{  "ko": "옴나이서" },
-        "Words of Mormon":{  "ko": "몰몬의 말씀" },
-        "Mosiah":{  "ko": "모사이야서" },
-        "Alma":{  "ko": "앨마서" },
-        "Helaman":{  "ko": "힐라맨서" },
-        "Hel\\.":{  "ko": "힐라맨서" },
-        "3 Nephi":{  "ko": "제3니파이" },
-        "3 Ne\\.":{  "ko": "제3니파이" },
-        "4 Nephi":{  "ko": "제4니파이" },
-        "4 Ne\\.":{  "ko": "제4니파이" },
-        "Mormon":{  "ko": "몰몬서" },
-        "Morm\\.":{  "ko": "몰몬서" },
-        "Ether":{  "ko": "이더서" },
-        "Moroni":{  "ko": "모로나이서" },
-        "Moro\\.":{  "ko": "모로나이서" }
-    };
-
-    let books = Object.keys(dictionary).join("|");
-    let pattern = new RegExp("("+books+")\\s*\\d+","g");
-    text = text.replace(pattern, (match, p1) => {
-        if(!dictionary[p1]?.[lang]) return match;
-        return match.replace(p1, dictionary[p1][lang]);
-    });
-
-    text = text.replace(/성경 *(구절|말씀)/g, "경전 구절");
-    text = text.replace(/이 문장/g, "이 구절");
-
-    return text;
-}
-
 
 
 
