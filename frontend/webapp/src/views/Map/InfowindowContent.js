26acb2f45c5ccdba89a1e13e3d79fb1095b23f41
KC Kern <kc@kckern.com>
2023-10-23 23:30:23 +0900

    Map CSS

--- frontend/webapp/src/views/Map/InfowindowContent.js
+++ frontend/webapp/src/views/Map/InfowindowContent.js
@@ -7,19 +7,33 @@ import { renderPersonPlaceHTML } from "../Page/PersonPlace";
 import ReactTooltip from "react-tooltip";
 
 export default function InfowindowContent({ placeInfo, push, appController }) {
+
+    if (!placeInfo) return <div>Loading...</div>;
+
     return (
         <BrowserRouter>
             <div className="map-info">
                 <div style={{ overflow: "auto" }}>
                     <div className="placebox">
                         <div className="ppbody">
+                            
+                            <div className="bodytext map-place-info">
                             <h3>
                                 {processName(placeInfo.name)}<br />
                                 <small className="map-place-brief-info">
                                     {replaceNumbers(placeInfo.info)}
                                 </small>
                             </h3>
+                                {renderPersonPlaceHTML(placeInfo.description, appController)}
+                            </div>
+
+
                             <div className="refbox">
+
+                            <div className="ppimg map-place-image">
+                                    <img src={`${assetUrl}/places/${placeInfo.slug}`} />
+                                </div>
+                                
                                 <h4>{label("references")}</h4>
                                 <ol className="sd">{
                                     placeInfo.index &&
@@ -41,12 +55,7 @@ export default function InfowindowContent({ placeInfo, push, appController }) {
                                     id='info-place-person'
                                 />
                             </div>
-                            <div className="bodytext map-place-info">
-                                <div className="ppimg map-place-image">
-                                    <img src={`${assetUrl}/places/${placeInfo.slug}`} />
-                                </div>
-                                {renderPersonPlaceHTML(placeInfo.description, appController)}
-                            </div>
+
                         </div>
                     </div>
                 </div>
