8d727706d0af797b2772d306b6d58ca18891319d
KC Kern <kc@kckern.com>
2023-10-29 08:00:06 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -67,11 +67,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //allow cors from all
+
+const allowedOrigins = [
+  'localhost', 
+  'bookofmormon.online', 
+  'xn--289a67xla.kr'
+];
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  const origin = Array.isArray(req.headers.origin) ? req.headers.origin[0] : req.headers.origin;
+  const isValidRegex = allowedOrigins.some((allowedOrigin) => {
+    const regex = new RegExp(`^https?://(*\.)*${allowedOrigin}(:[0-9]+)*?$`);
+    return regex.test(origin);
+  });
+
+  if (isValidRegex) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 
 app.all("/ping", ping);
 
