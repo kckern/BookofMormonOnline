fb79d75148007fdab868b1b7a6fd3fddf6d3eaf2
KC Kern <kc@kckern.com>
2023-10-29 21:10:27 +0900

    Update apollo.ts

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -4,7 +4,7 @@ import resolvers from '../resolvers';
 import { isArray } from 'util';
 import { sequelize } from './database';
 
-const langs = ["en","fr","de","nl","pt","ko","jpn","zh","ru","hi","eo","es","vn","tgl","th","ukr","tam"];
+const langs = ["en","fr","de","nl","pt","ko","jpn","zh","ru","hi","eo","es","vn","tgl","th","ukr","tam","swe"];
 
 
 
