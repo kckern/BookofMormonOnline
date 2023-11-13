649d9b74e6e8373dd59344c13fdbd0cbecf61ff6
KC Kern <kc@kckern.com>
2023-11-13 11:18:42 +0900

    New user Avatar

--- src/resolvers/lib.js
+++ src/resolvers/lib.js
@@ -273,8 +273,85 @@ async function organizeRelatedScriptures(scriptureDataArray)
     return scriptureDataArray;
 }
 
+const pickOneRamdomly = (arr) => {
+
+    const selectedIndex = Array.from(Array(arr.length).keys()).sort(() => Math.random() - 0.5).pop();
+    return arr[selectedIndex];
+}
+
+async function genUserAvatar(user_id) {
+    const pallettes =  
+    [
+      ["FF86F1", "FF00CC"], // Baby Pink to Deep Pink
+      ["00FFFF", "000080"], // Aqua to Navy
+      ["99FFCC", "009933"], // Mint Cream to Pakistan Green
+      ["FF6699", "990033"], // Thulian Pink to Smokey Topaz
+      ["33CCFF", "003366"], // Vivid Sky Blue to Smoky Black
+      ["00FF80", "004D40"], // Spring Green to Deep Jungle Green
+      ["FF9933", "6B4423"], // Neon Carrot to Brown Pod
+      ["FF99FF", "993399"], // Pink Flamingo to Tyrian Purple
+      ["99CCFF", "003399"], // Light Cornflower Blue to Dark Powder Blue
+      ["00CC99", "006633"], // Caribbean Green to MSU Green
+      ["FF9999", "800000"], // Light Salmon to Maroon
+      ["FFFF99", "808000"], // Canary to Olive
+      ["99FF99", "006400"], // Witches Green to Pakistan Green
+      ["FFCC99", "8B4513"], // Peach Orange to Saddle Brown
+      ["CCCCFF", "000099"], // Periwinkle to Duke Blue
+      ["CC99FF", "660099"], // Light Pastel Purple to Blue-violet
+      ["FF66CC", "660033"], // Carnation Pink to Rosewood
+      ["CCFFFF", "006666"], // Pale Cyan to Midnight Green
+      ["FF9966", "663300"], // Atomic Tangerine to Zinnwaldite Brown
+      ["66CCFF", "002266"], // Sky Blue to Navy blue
+      ["99CC66", "435D36"], // Asparagus to Rifle Green
+      ["66FF66", "003300"], // Screamin' Green to Dark Green
+      ["FFFF66", "878700"], // Yellow to Olive Green
+      ["FF9999", "942121"], // Light Salmon Pink to Dark Sienna
+      ["FFCCCC", "853333"], // Bubble Gum to Deep Chestnut
+      ["99CC99", "385438"], // Pale Green to British Racing Green
+      ["CCCC99", "545400"], // Pale Olive to Army Green
+      ["CCFFCC", "004700"], // Mint Cream to Dartmouth Green
+      ["FFCC99", "6B3600"], // Peach-orange to Brown
+      ["CCFF99", "3B5900"], // Pale Lime to Olive Drab
+      ["FFFFCC", "878600"], // Cream to Dark Yellow
+      ["FF9966", "803000"], // Atomic Tangerine to Mahogany
+      ["CCFF66", "315000"], // Lime Green to Lush Pine
+      ["FFCC66", "804000"], // Mango Tango to Caput Mortuum
+      ["CC9999", "541717"], // Copper Rose to Dark Sienna
+      ["CC66FF", "440088"], // Heliotrope to Violet
+      ["FF66CC", "880044"], // Carnation Pink to Byzantium
+      ["CC9966", "543517"], // Wood Brown to Liver
+      ["FF66FF", "880088"], // Shocking Pink to Purple
+      ["FFCCCC", "884444"], // Bubble Gum to Redwood
+      ["CCFFCC", "004400"], // Mint Cream to British Racing Green
+      ["CCCCFF", "000088"], // Light Periwinkle to Dark Blue
+      ["99CCFF", "002288"], // Pale Cyan to Sapphire
+      ["CC99CC", "541754"], // Lilac to Tyrian Purple
+      ["9933CC", "320066"], // Dark Orchid to Ultra Purple
+      ["3333CC", "000066"], // Medium Blue to Blue Black
+      ["0099CC", "002266"], // Bondi Blue to Navy Blue
+      ["6699CC", "001544"], // Jordy Blue to Oxford Blue
+      ["CC3366", "54001A"], // Fuchsia Rose to Blackberry
+      ["009933", "00260D"], // Shamrock Green to British Racing Green
+      ["669966", "001400"]  // Camouflage Green to Dark Green
+    ];
+
+    const pack = "thumbs"; //icons, shapes, rings
+    const seed = user_id.slice(0, 5);
+    const [back,fore] = pickOneRamdomly(pallettes);
+    const mouths = ["variant1","variant2","variant3","variant4"];
+    const rotations = ["0", "20", "340", "40", "320"];
+    const eyes = "variant6W10,variant8W14,variant2W10";
+    const backgroundColor = `backgroundColor=${back}`;
+    const shapeColor = `shapeColor=${fore}`;
+    const rotation = `rotate=${pickOneRamdomly(rotations)}`
+    const mouth = `mouth=${pickOneRamdomly(mouths)}`;
+    const url = `https://api.dicebear.com/7.x/${pack}/svg?seed=${seed}&${backgroundColor}&${shapeColor}&eyes=${eyes}&${rotation}&scale=70&${mouth}`;
+    return url;
+
+}
+
 
 
 
 //export
-module.exports = {getBlocksToQueue,getFirstTextBlockGuidFromSlug,organizeRelatedScriptures}
+module.exports = {getBlocksToQueue,getFirstTextBlockGuidFromSlug,organizeRelatedScriptures,genUserAvatar}
