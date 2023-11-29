8a1ba2bc3708b614cc980d8d62d60da04f12518d
KC Kern <kc@kckern.com>
2023-11-29 16:16:07 +0900

    Update lib.js

--- src/resolvers/lib.js
+++ src/resolvers/lib.js
@@ -275,13 +275,11 @@ async function organizeRelatedScriptures(scriptureDataArray)
 }
 async function processPassages(verse_ids, verse_data,lang)
 {
-    const versions = ['MEV', 'SEBOM', 'REDC']; //todo: check language
-
     const firstVerse = verse_ids[0];
     let placeholders = new Array(verse_ids.length).fill('?').join(',');
-    const headingSQL = `(SELECT verse_id,text FROM \`scripture.guide\`.scripture_headings WHERE version IN (?, ?, ?) AND verse_id <= ? AND type = 'heading' ORDER BY verse_id DESC LIMIT 1)
-    UNION (SELECT verse_id,text FROM \`scripture.guide\`.scripture_headings WHERE version IN (?, ?, ?) AND verse_id IN (${placeholders}) AND type = 'heading' ORDER BY verse_id )`;   
-    const params = [...versions, firstVerse, ...versions, ...verse_ids];    
+    const headingSQL = `(SELECT verse_id, text FROM lds_scriptures_headings WHERE verse_id <= ? AND lang = 'en' ORDER BY verse_id DESC LIMIT 1)
+    UNION (SELECT verse_id, text FROM lds_scriptures_headings WHERE verse_id IN (${placeholders}) AND lang = 'en' ORDER BY verse_id)`;
+    const params = [firstVerse,  ...verse_ids];    
     let headingData = await queryDB(headingSQL, params);
     headingData = headingData.filter((v,i,a)=>a.findIndex(t=>(t.verse_id === v.verse_id))===i);
     return headingData.sort((a, b) => a.verse_id - b.verse_id)
