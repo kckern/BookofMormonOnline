86c47d823456f87529e1be62c94191bd60a3c6de
Владислав Бондарчук <crm.30235@dz.com>
2023-12-06 11:00:27 +0200

    feature:Sendbird SDK 4.0 last changes

--- frontend/webapp/src/views/_Common/Study/DirectMessages.js
+++ frontend/webapp/src/views/_Common/Study/DirectMessages.js
@@ -8,26 +8,33 @@ export default function DirectMessages({ appController, userId }) {
   const theirId = userId;
 
   const [channel, setChannel] = useState(null);
-  if (channel === null || !channel?.memberMap[theirId]) {
+  if (
+    channel === null ||
+    !channel?.members.find((member) => member.userId === theirId)
+  ) {
     //https://sendbird.com/docs/chat/v3/javascript/guides/group-channel#2-create-a-channel
 
-    var params = new appController.sendbird.sb.GroupChannelParams();
+    var params = {};
     params.isDistinct = true;
-    params.addUserIds([myId, theirId]);
+    params.invitedUserIds = [myId, theirId];
     params.name = `DM between ${myId} and ${theirId}`;
     params.customType = "DM";
     params.channelUrl = md5(); // In a group channel, you can create a new channel by specifying its unique channel URL in a 'GroupChannelParams' object.
 
-    appController.sendbird.sb.GroupChannel.createChannel(
-      params,
-      function (groupChannel, error) {
-        if (error) {
-          console.log("createChannelWithUserIds.ERROR", { error });
-        }
-        setChannel(groupChannel);
-      }
+    try {
+      appController.sendbird.sb.groupChannel
+        .createChannel(params)
+        .then((groupChannel) => {
+          setChannel(groupChannel);
+        });
+    } catch (error) {
+      console.log("createChannelWithUserIds.ERROR", { error });
+    }
+    return (
+      <div className={"StudyGroupChatPanel "}>
+        <Loader />
+      </div>
     );
-    return <div className={"StudyGroupChatPanel "}><Loader/></div>;
   }
   return (
     <StudyGroupChatPanel channel={channel} appController={appController} />
