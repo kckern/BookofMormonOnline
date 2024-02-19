2a2e5c3acc6e42cdfa409007d13019b11575c84f
KC Kern <kc@kckern.com>
2024-02-18 21:22:06 -0500

    Marker Types

--- frontend/webapp/src/views/Map/MapContents.js
+++ frontend/webapp/src/views/Map/MapContents.js
@@ -1,55 +1,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import BoMOnlineAPI, { assetUrl } from "../../models/BoMOnlineAPI";
-
-
-const CanvasMarker = ({ name, style }) => {
-
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
-    const fontSize = 20;
-    const fontString = `${fontSize}px 'Roboto Condensed'`;
-
-    const iconPadding = 5;
-    context.font = fontString;
-    let metrics = context.measureText(name);
-    const textWidth = metrics.width;
-    let fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
-    const diff = fontHeight - fontSize;
-    const iconWidth = fontHeight;
-
-
-    canvas.width = textWidth + iconWidth + iconPadding;
-    canvas.height = fontHeight;
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(iconWidth, 0);
-    context.lineTo(iconWidth/2, iconWidth);
-    context.closePath();
-    context.fillStyle = style.fill;
-    context.fill();
-
-
-    //set the font
-    context.font = fontString;
-    
-    context.lineWidth = 3;
-    context.strokeStyle = "#FFFFFF"; // Use strokeStyle for the outline
-    context.strokeText(name, iconWidth + iconPadding, fontHeight - (diff *2) ); // Draw the outline
-
-    //thin black text, no stroke
-    context.lineWidth = 1;
-    context.fillStyle = "#000000"; // Fill should be black
-    context.fillText(name, iconWidth + iconPadding, fontHeight - (diff *2)); // Draw the filled text
-
-
-    
-
-    return canvas.toDataURL();
-
-};
-
-
+import { CanvasMarker } from "./MapMarkers";
 
 
 const MapContents = () => {
@@ -61,93 +13,95 @@ const MapContents = () => {
     const mapslug = "baja";
     const mapCenter = [-113.337377, 27.047839];
     const mapBounds = [-117.729321, 22.298062, -108.945432, 31.797617];
-    const minZoom = 4;
+    const minZoom = 6;
     const maxZoom = 9;
     const iniZoom = 6;
 
     const markers = [
-        {  name: "New York", coords: [-113.337377, 27.047839], style: { fill: "#6b7d91" }  },
-        {  name: "Zarahemla", coords: [-113.337377, 28.047839], style: { fill: "#6b7d91" }  },
-        {  name: "서울", coords: [-111.337377, 29.047839], style: { fill: "#6b7d91" }  },
-        {  name: "Land of Nephi", coords: [-112.337377, 26.047839], style: { fill: "#6b7d91" }  },
+        {  name: "New York", xy: [-113.337377, 27.047839], location_type: "city"  },
+        {  name: "City Sea", xy: [-113.337377, 30.047839], location_type: "city_right"  },
+        {  name: "Zarahemla", xy: [-113.337377, 28.047839], location_type: "land"  },
+        {  name: "서울", xy: [-111.337377, 29.047839], location_type: "region"  },
+        {  name: "Land of Nephi", xy: [-112.337377, 26.047839], location_type: "geo"  },
+        {  name: "Sea/South", xy: [-112.337377, 25.047839], location_type: "aqua"  },
     ]
     .map(i=>{
-        const marker = new window.ol.Feature({
-            geometry: new window.ol.geom.Point(window.ol.proj.fromLonLat(i.coords)),
-            anchor: [0.5, 1],
 
-        });
-    
-        let iconStyle = new window.ol.style.Style({
-            image: new window.ol.style.Icon({
-                src: CanvasMarker({...i}),
-                scale: 1,
-            })
-        });
-    
-       marker.setStyle(iconStyle);
-    
+        const {minResolution, maxResolution} = i;
+        const [height, width, anchor, src] = CanvasMarker({...i});
+        const geometry  = new window.ol.geom.Point(window.ol.proj.fromLonLat(i.xy));
+        const image     = new window.ol.style.Icon({ src, size: [width, height], anchor });
+        let iconStyle   = new window.ol.style.Style({image, minResolution, maxResolution});
+        const marker    = new window.ol.Feature({ geometry });
+        marker.setStyle(iconStyle);
         return marker;
     });
+    
+const drawMap = ()=>{
+
+    if (!window.ol) return console.error('OpenLayers library not found');
+    
+    map.current = new window.ol.Map({
+        target: mapElement.current,
+        layers: [new window.ol.layer.Tile({
+            extent: window.ol.proj.transformExtent(mapBounds, 'EPSG:4326', 'EPSG:3857'),
+            source: new window.ol.source.XYZ({
+                url: `${assetUrl}/maptiles/${mapslug}/{z}/{x}/{y}`, 
+                tilePixelRatio: 1, minZoom,  maxZoom
+            })
+        })],
+        view: new window.ol.View({
+            center: window.ol.proj.fromLonLat(mapCenter),
+            zoom: iniZoom,
+            minZoom: minZoom,
+            maxZoom: maxZoom
+        })
+    });
+
+    map.current.addLayer(new window.ol.layer.Vector({ source: new window.ol.source.Vector({ features: [...markers] }) }));
+
+
+
+    //onhover cursor pointer
+    map.current.on('pointermove', function(e) {
+        if (e.dragging) return;
+        var pixel = map.current.getEventPixel(e.originalEvent);
+        var hit = map.current.forEachFeatureAtPixel(pixel, function(feature) {
+            return true;
+        });
+        map.current.getTargetElement().style.cursor = hit ? 'pointer' : '';
+    });
+
 
     
 
-    useEffect(() => {
-        if (!window.ol) return console.error('OpenLayers library not found');
-        
-        map.current = new window.ol.Map({
-            target: mapElement.current,
-            layers: [new window.ol.layer.Tile({
-                extent: window.ol.proj.transformExtent(mapBounds, 'EPSG:4326', 'EPSG:3857'),
-                source: new window.ol.source.XYZ({
-                    url: `${assetUrl}/maptiles/${mapslug}/{z}/{x}/{y}`, 
-                    tilePixelRatio: 1, minZoom,  maxZoom
-                })
-            })],
-            view: new window.ol.View({
-                center: window.ol.proj.fromLonLat(mapCenter),
-                zoom: iniZoom,
-            })
+
+
+    // ADMIN ONLY: On marker Drag Function, Move the marker and get the new coordinates
+    if(isAdmin){
+        var modify = new window.ol.interaction.Modify({ 
+            features: new window.ol.Collection(markers)
         });
-
-        map.current.addLayer(new window.ol.layer.Vector({ source: new window.ol.source.Vector({ features: [...markers] }) }));
-
-
-
-        //onhover cursor pointer
-        map.current.on('pointermove', function(e) {
-            if (e.dragging) return;
-            var pixel = map.current.getEventPixel(e.originalEvent);
-            var hit = map.current.forEachFeatureAtPixel(pixel, function(feature) {
-                return true;
-            });
-            map.current.getTargetElement().style.cursor = hit ? 'pointer' : '';
-        });
+        modify.on('modifyend', (e)=>alert(JSON.stringify(e.features.getArray()[0].getGeometry().getCoordinates())));
+        map.current.addInteraction(modify);
+    }
 
 
-        
+    return () => { 
+        if (map.current) map.current.setTarget(undefined);  
+        map.current.removeInteraction(modify);
+    };
 
 
+}
 
-        // ADMIN ONLY: On marker Drag Function, Move the marker and get the new coordinates
-        if(isAdmin){
-            var modify = new window.ol.interaction.Modify({ 
-                features: new window.ol.Collection(markers)
-            });
-            modify.on('modifyend', (e)=>alert(JSON.stringify(e.features.getArray()[0].getGeometry().getCoordinates())));
-            map.current.addInteraction(modify);
-        }
-
-
-        return () => { 
-            if (map.current) map.current.setTarget(undefined);  
-            map.current.removeInteraction(modify);
-        };
-
-
-    }, []);
+    useEffect(drawMap, []);
 
     return <div id="map" ref={mapElement} ></div>
 };
+
+
+
+
 
 export default MapContents;
